000a4d304371f3f43b5e285072a6dce9665da9a4
aniyagnik <ani789yagnik@gmail.com>
2022-09-14 22:02:15 +0530

    add

--- middleware/db.js
+++ middleware/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const mongoLink = process.env.DATABASE_URI
+const mongoLink = "mongodb://project:projectQ12@cluster0-shard-00-00-6zit8.mongodb.net:27017,cluster0-shard-00-01-6zit8.mongodb.net:27017,cluster0-shard-00-02-6zit8.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority"
 // connecting db
 const connectDb = async () => {
   try {
